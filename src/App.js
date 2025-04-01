fe96d88d740698ad0fca3a07fed5d8a5f3a4cfb6
NewtonKamau <kamaunewton78@gmail.com>
2025-04-01 15:55:30 +0300

    add configuration files for ESLint and Prettier, and update App component formatting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-const Pizza = (props) => { 
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.name || "Pizza"),
-        React.createElement("p", {}, props.description || "Delicious pizza"),
-    ]);
-}
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name || "Pizza"),
+    React.createElement("p", {}, props.description || "Delicious pizza"),
+  ]);
+};
 
-const App = () => { 
-    return React.createElement("div",
-        {},
-        [
-            React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza, {name: "Margherita", description: "Tomato, mozzarella, basil"}),
-            React.createElement(Pizza, { name: "Pepperoni", description: "Tomato, mozzarella, pepperoni" }),
-            React.createElement(Pizza, { name: "Hawaiian", description: "Tomato, mozzarella, ham, pineapple" }),
-            React.createElement(Pizza, { name: "Vegetarian", description: "Tomato, mozzarella, vegetables" }),
-            React.createElement(Pizza, { name: "BBQ Chicken", description: "BBQ sauce, mozzarella, chicken" }),
-        ]
-    )
-}
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "Margherita",
+      description: "Tomato, mozzarella, basil",
+    }),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "Tomato, mozzarella, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawaiian",
+      description: "Tomato, mozzarella, ham, pineapple",
+    }),
+    React.createElement(Pizza, {
+      name: "Vegetarian",
+      description: "Tomato, mozzarella, vegetables",
+    }),
+    React.createElement(Pizza, {
+      name: "BBQ Chicken",
+      description: "BBQ sauce, mozzarella, chicken",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
