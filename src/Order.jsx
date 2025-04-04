ebc3bd602065d88adbb5651bda7f9077375bd93b
NewtonKamau <kamaunewton78@gmail.com>
2025-04-04 10:52:38 +0300

    refactor App component to use JSX and create Order component for pizza ordering

---
+++ src/Order.jsx
@@ -0,0 +1,66 @@
+import Pizza from "./Pizza";
+export default function Order() {
+  const pizzaType = "Margherita";
+  const pizzaSize = "M";
+  return (
+    <div className="order">
+      <h2>Create Order </h2>
+      <form>
+        <div>
+          <div>
+            <label htmlFor="pizza-type">Pizza Type</label>
+            <select id="pizza-type" name="pizza-type" value={pizzaType}>
+              <option value="Margherita">Margherita</option>
+              <option value="Pepperoni">Pepperoni</option>
+              <option value="Hawaiian">Hawaiian</option>
+            </select>
+            <div>
+              <label htmlFor="pizza-size">Pizza Size</label>
+              <div>
+                <span>
+                  <input
+                    type="radio"
+                    id="small"
+                    name="pizza-size"
+                    value="S"
+                    checked={pizzaSize === "S"}
+                  />
+                  <label htmlFor="small">Small</label>
+                </span>
+                <span>
+                  <input
+                    type="radio"
+                    id="medium"
+                    name="pizza-size"
+                    value="M"
+                    checked={pizzaSize === "M"}
+                  />
+                  <label htmlFor="medium">Medium</label>
+                </span>
+                <span>
+                  <input
+                    type="radio"
+                    id="large"
+                    name="pizza-size"
+                    value="L"
+                    checked={pizzaSize === "L"}
+                  />
+                  <label htmlFor="large">Large</label>
+                </span>
+              </div>
+            </div>
+            <button type="submit">Add to cart</button>
+            <div className="order-pizza">
+              <Pizza
+                name="Pepperoni"
+                description="Tomato, mozzarella, pepperoni"
+                image="/public/pizzas/pepperoni.webp"
+              />
+              <p>$13.21</p>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
