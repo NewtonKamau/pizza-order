5a9730df048b6c6193c5d5e91eddec3fb63a0aca
NewtonKamau <kamaunewton78@gmail.com>
2025-04-17 11:26:00 +0300

    add CartContext and integrate Header component for cart state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import {useState, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import Header from "./Header";
+import { CartContext } from "./contexts";
 import { PizzaOfTheDay } from "./PizzaOfTheDay";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1 className="logo">Padre Gino's Pizza</h1>
-      <Order />
-      <PizzaOfTheDay />
-    </div>
+    <StrictMode>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+          <footer>
+            <p>
+              Made with ❤️ by{" "}
+              <a href="https://github.com/newtonkamau">Newton</a>
+            </p>
+          </footer>
+        </div>
+      </CartContext.Provider>
+    </StrictMode>
   );
 };
 
